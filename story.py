eab8479c8eff9a97fb8bfbf79106908fbc021cc4
sashaodumodu <odumodusasha@gmail.com>
2025-09-16 14:30:01 -0400

    Added a third print statement for the hero path

--- story.py
+++ story.py
@@ -11,6 +11,7 @@ def intro():
 def left_path():
     print("You walk left and find a mysterious glowing sword stuck in a stone.")
     print("You pull the sword out quickly, with ease, surprised at how light it felt.")
+    print("Suddenly, you're confronted by a restless dragon. It charges you, but you quickly slay it.")
 
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
