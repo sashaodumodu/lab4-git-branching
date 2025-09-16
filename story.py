d0a12ab5284cacf8a2cb1dd3d48a4932a273fdc3
sashaodumodu <odumodusasha@gmail.com>
2025-09-16 14:05:48 -0400

    Added a second print statement.

--- story.py
+++ story.py
@@ -10,6 +10,8 @@ def intro():
 
 def left_path():
     print("You walk left and find a mysterious glowing sword stuck in a stone.")
+    print("You pull the sword out quickly, with ease, surprised at how light it felt.")
+    print("Suddenly confront with a restless dragon, it charges you, but you quickly slay it.")
 
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
